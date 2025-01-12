63f36533b0e53b0f5a9bb91b00ebdc8f8b87a2da
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-12 19:51:59 +0800

    globals

---
+++ src/data/globals.ts
@@ -0,0 +1,27 @@
+export const navItems = [
+  {
+    id: 1,
+    label: "Home",
+    href: "#",
+  },
+  {
+    id: 2,
+    label: "Market Trends",
+    href: "#",
+  },
+  {
+    id: 3,
+    label: "Trends and Analysis",
+    href: "#",
+  },
+  {
+    id: 4,
+    label: "News",
+    href: "#",
+  },
+  {
+    id: 5,
+    label: "About us",
+    href: "#",
+  },
+];
