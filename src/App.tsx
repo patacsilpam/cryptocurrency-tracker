11204d44010eb984f84f97a2b22868bcfdb4b0d3
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-16 23:01:32 +0800

    feat: make the website responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { navItems } from "./data/globals";
 import { Navbar } from "./components/layout/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Hero } from "./components/layout/Hero";
 function App() {
   return (
-    <>
+    <div className="relative flex flex-col justify-center  items-center overflow-hidden max-w-7xl w-full mx-auto px-10">
       <Router>
         <Navbar navItems={navItems} />
+        <Hero />
         <Routes></Routes>
       </Router>
-    </>
+    </div>
   );
 }
 
