e0afee7a8a76e7abc13c4bb15e0c55d2e7f5d1a4
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-22 22:23:43 +0800

    refactor: removed item-center for designing flexibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/routes";
 function App() {
   return (
-    <div className="relative flex flex-col justify-center  items-center overflow-hidden max-w-7xl w-full mx-auto px-10  text-white">
+    <div className="relative flex flex-col justify-center overflow-hidden max-w-7xl w-full mx-auto px-10  text-white">
       <RouterProvider router={router} />
     </div>
   );
