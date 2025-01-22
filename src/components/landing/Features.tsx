3cc2b29e839c6fa32d29a7089214980d9730f3d9
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-22 22:21:03 +0800

    feat: app feature component

--- src/components/landing/Features.tsx
+++ src/components/landing/Features.tsx
@@ -1,20 +1,46 @@
-export function Features() {
+export function Features({
+  featureItems,
+}: {
+  featureItems: Array<{
+    id: number;
+    icon: string; // Optional JSX.Element for the icon
+    label: string;
+    description: string;
+  }>;
+}) {
   return (
-    <>
-      <section>
-        <section>
-          <h1>Stay Ahead with Real-Time Crypto Insights</h1>
-        </section>
-        <section>
-          <p>
-            Crypto Daily offers cutting-edge tools for tracking cryptocurrency
-            prices in real-time. Our platform provides comprehensive market
-            trends and in-depth analysis, empowering you to make informed
-            investment decisions. Whether you're a trader or a tech enthusiast,
-            our features cater to your needs.
-          </p>
-        </section>
+    <section id="features" className="flex flex-col space-y-12 h-[80vh]">
+      {/* Header Section */}
+      <section className="flex flex-row items-start justify-between gap-5 md:gap-32">
+        <p className="text-3xl md:text-4xl lg:text-4xl font-medium text-[#dddfe4] w-full md:w-1/2">
+          Stay Ahead with Real-Time Crypto Insights
+        </p>
+        <p className="text-gray-400 w-full md:w-1/2">
+          Crypto Daily offers cutting-edge tools for tracking cryptocurrency
+          prices in real-time. Our platform provides comprehensive market trends
+          and in-depth analysis, empowering you to make informed investment
+          decisions.
+        </p>
       </section>
-    </>
+
+      {/* Features Section */}
+      <section className="space-y-10">
+        <div className="flex flex-col md:flex-row justify-around">
+          {featureItems.map((item) => (
+            <div
+              key={item.id}
+              className="  bg-zinc-800 bg-clip-padding backdrop-filter  backdrop-blur bg-opacity-55 backdrop-saturate-50 backdrop-contrast-125 p-6 rounded-lg mr-5 h-56 my-3 md:my-0"
+            >
+              {/* Render icon if provided */}
+              {item.icon && <div className="py-2">{item.icon}</div>}
+              {/* Render label and description */}
+              <p className="text-xl font-bold py-2">{item.label}</p>
+
+              <p className="text-md text-gray-400">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </section>
   );
 }
