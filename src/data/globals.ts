25856994a7c347e4cb93aaf87ec924ad23533634
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-22 22:25:16 +0800

    feat: added icon to features

--- src/data/globals.ts
+++ src/data/globals.ts
@@ -1,3 +1,5 @@
+import { Clock, ChartNoAxesCombined, BetweenVerticalEnd } from "lucide-react";
+import React from "react";
 export const navItems = [
   {
     id: 1,
@@ -23,5 +25,27 @@ export const navItems = [
     id: 5,
     label: "FAQs",
     href: "FAQs",
+  },
+];
+
+export const featureItems = [
+  {
+    id: 1,
+    icon: React.createElement(Clock),
+    label: "Real-Time Cryptocurrency Price Tracking",
+    description: "Get instant updates on cryptocurrency prices as they happen.",
+  },
+  {
+    id: 2,
+    icon: React.createElement(ChartNoAxesCombined),
+    label: "Explore Market Trends and Insights",
+    description:
+      "Analyze market movements to identify profitable opportunities.",
+  },
+  {
+    id: 3,
+    icon: React.createElement(BetweenVerticalEnd),
+    label: "Advanced Analysis Tools for Informed Decisions",
+    description: "Utilize our tools to enhance your trading strategies.",
   },
 ];
