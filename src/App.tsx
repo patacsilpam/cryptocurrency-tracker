bda8b66fe17e1425583972041b3cb66dbb85296e
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-25 23:19:12 +0800

    refactor: removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/routes";
 import { FetchCoins } from "./hooks/fetch";
 function App() {
-  FetchCoins();
   return (
     <div className="relative flex flex-col justify-center overflow-hidden max-w-7xl w-full mx-auto px-10  text-white">
       <RouterProvider router={router} />
