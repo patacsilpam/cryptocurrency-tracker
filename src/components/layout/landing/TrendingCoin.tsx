50132fea15424bde5722db65815aaab09c74cbd5
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-26 19:05:52 +0800

    feat: added table component for showing list of trending coins

--- src/components/layout/landing/TrendingCoin.tsx
+++ src/components/layout/landing/TrendingCoin.tsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
 import { formattedPrice } from "../../../utils/utils";
-export function TrendingCoin() {
-  interface Coin {
+import { Coin } from "../../../interfaces/interface";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../common/table";
+export interface CoinResponse {
+  coins: {
     item: {
       id: string;
       coin_id: string;
       name: string;
       symbol: string;
-      market_cap_rank: number;
       thumb: string;
       data: {
         price: number;
+        market_cap: string;
       };
     };
-  }
-
+  }[];
+}
+export function TrendingCoin() {
   const [trendingCoins, setTrendingCoins] = useState<Coin[]>([]);
   useEffect(() => {
     const fetchTrendingCoins = async () => {
@@ -32,9 +44,21 @@ export function TrendingCoin() {
         if (!response.ok) {
           throw new Error(`Response status:${response.status}`);
         }
-        const json = await response.json();
+        const data: CoinResponse = await response.json();
 
-        setTrendingCoins(json.coins);
+        setTrendingCoins(
+          data.coins.map((coin) => ({
+            id: coin.item.id,
+            coin_id: coin.item.coin_id,
+            name: coin.item.name,
+            symbol: coin.item.symbol,
+            thumb: coin.item.thumb,
+            data: {
+              price: coin.item.data.price,
+              market_cap: coin.item.data.market_cap,
+            },
+          }))
+        );
       } catch (error) {
         console.error("Fetch error:", error);
       }
@@ -46,26 +70,31 @@ export function TrendingCoin() {
       <div>
         <h1>Trending Coins</h1>
         <div className="flex flex-col">
-          {trendingCoins.map((coin) => (
-            <div
-              key={coin.item.coin_id}
-              className="flex flex-row items-center space-x-5"
-            >
-              <div>
-                <img
-                  src={coin.item.thumb}
-                  alt={coin.item.name}
-                  className="h-16"
-                  loading="lazy"
-                />
-              </div>
-              <div>
-                <p>{coin.item.name}</p>
-                <p> {coin.item.symbol}</p>
-                <p>{formattedPrice(coin.item.data.price)}</p>
-              </div>
-            </div>
-          ))}
+          <Table>
+            <TableCaption>Trending Coins</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>#</TableHead>
+                <TableHead className="w-[300px]">Coin</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Market Cap</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {trendingCoins.map((item, index) => (
+                <TableRow key={item.coin_id}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell className="font-medium flex items-center gap-2">
+                    <img src={item.thumb} alt={item.name} className="h-7" />{" "}
+                    {item.name}
+                    <small className="text-slate-400">{item.symbol}</small>
+                  </TableCell>
+                  <TableCell>{formattedPrice(item.data.price)}</TableCell>
+                  <TableCell>{item.data.market_cap}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </>
