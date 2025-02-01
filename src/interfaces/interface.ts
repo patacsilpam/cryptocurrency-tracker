e37e9a5105300db5ee6741325ac1e297d74187f6
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-01 22:42:29 +0800

    refactor: optimize interfaces by extracting common properties into base properties

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,4 +1,4 @@
-export interface Coin {
+/*export interface Coin {
   id: string;
   coin_id: string;
   name: string;
@@ -58,4 +58,47 @@ export interface NFTResponse {
       sparkline: string;
     };
   }[];
+}
+*/
+
+interface BaseAsset {
+  id: string;
+  name: string;
+  symbol: string;
+  thumb: string;
+}
+// interface for trending coins
+interface CoinData {
+  price: number;
+  market_cap: string;
+  sparkline: string;
+  price_change_percentage_24h: {
+    usd: number;
+  };
+}
+
+export interface Coin extends BaseAsset {
+  coin_id: string;
+  data: CoinData;
+}
+
+export interface CoinResponse {
+  coins: {
+    item: Coin;
+  }[];
+}
+
+//interface for trending NFTs
+interface NFTData {
+  floor_price: number;
+  floor_price_in_usd_24h_percentage_change: string;
+  sparkline: string;
+}
+
+export interface NFT extends BaseAsset {
+  data: NFTData;
+}
+
+export interface NFTResponse {
+  nfts: NFT[];
 }
