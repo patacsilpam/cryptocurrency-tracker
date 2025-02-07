f6cfef541b9a22eeba4e964c5dd892f21929a9be
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-07 13:30:11 +0800

    changed height

--- src/components/landing/Testimonials.tsx
+++ src/components/landing/Testimonials.tsx
@@ -3,8 +3,12 @@ import { testimonialsItem } from "../../data/globals";
 export const Testimonials = () => {
   return (
     <>
+      <div className="text-center py-10 space-y-3">
+      <h1 className="text-2xl font-medium">Loved by thousands of people</h1>
+      <p>Here's what some of our users have to say about Aceternity UI.</p>
+      </div>
       <div className=" grid md:grid-cols-3 grid-cols-1 gap-x-20  [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-50px),transparent_100%)]">
-        <div className="relative h-[90vh] overflow-hidden ">
+        <div className="relative h-[50vh] overflow-hidden ">
           {/* Content to scroll */}
           <div className="absolute top-0 left-0 w-full animate-marquee space-y-10 py-5">
             {testimonialsItem.map(
@@ -39,7 +43,7 @@ export const Testimonials = () => {
             )}
           </div>
         </div>
-        <div className="relative  h-[90vh] overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+        <div className="relative h-[50vh] overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
           {/* Content to scroll */}
           <div className="absolute top-0 left-0 w-full animate-marquee space-y-10 py-5">
             {testimonialsItem.map(
@@ -74,7 +78,7 @@ export const Testimonials = () => {
             )}
           </div>
         </div>
-        <div className="relative h-[90vh] overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+        <div className="relative h-[50vh] overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
           {/* Content to scroll */}
           <div className="absolute top-0 left-0 w-full animate-marquee space-y-10 py-5">
             {testimonialsItem.map(
