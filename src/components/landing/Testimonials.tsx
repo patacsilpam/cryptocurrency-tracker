56a42cf86ed472d3257bd958dfc7dccfe644290b
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-08 23:21:11 +0800

    refactor: reduced redundancy of the code

--- src/components/landing/Testimonials.tsx
+++ src/components/landing/Testimonials.tsx
@@ -1,116 +1,38 @@
-import test from "node:test";
 import { testimonialsItem } from "../../data/globals";
+import { TestimonialCard } from "./TestimonialsCard";
 export const Testimonials = () => {
   return (
     <>
-      <div className="text-center py-10 space-y-3">
-      <h1 className="text-2xl font-medium">Loved by thousands of people</h1>
-      <p>Here's what some of our users have to say about Aceternity UI.</p>
+      <div className=" text-center py-10 space-y-3 ">
+        <h1 className="text-2xl font-medium">Loved by thousands of people</h1>
+        <p>Here's what some of our users have to say about Crypto Daily.</p>
       </div>
-      <div className=" grid md:grid-cols-3 grid-cols-1 gap-x-20  [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-50px),transparent_100%)]">
-        <div className="relative h-[50vh] overflow-hidden ">
+      <div className="h-screen grid md:grid-cols-3 grid-cols-1 gap-x-20  [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-50px),transparent_100%)]">
+        <div className="relative  overflow-hidden ">
           {/* Content to scroll */}
-          <div className="absolute top-0 left-0 w-full animate-marquee space-y-10 py-5">
-            {testimonialsItem.map(
-              (item) =>
-                item.category === "Crypto Investor" &&
-                item.testimonies.map((testimony) => (
-                  <div
-                    key={testimony.id}
-                    className="bg-[#18181b] border border-[#282828] p-2 rounded-lg"
-                  >
-                    <p>{testimony.name}</p>
-                    <p>{testimony.role}</p>
-                    <p>{testimony.testimony}</p>
-                  </div>
-                ))
-            )}
+          <div className="absolute top-0 left-0 w-full animate-marquee space-y-10 py-5 ">
+            <TestimonialCard userCategory="Crypto Investor" />
           </div>
           <div className="absolute top-0 left-0 w-full animate-marquee2 space-y-10 py-5">
-            {testimonialsItem.map(
-              (item) =>
-                item.category === "Crypto Investor" &&
-                item.testimonies.map((testimony) => (
-                  <div
-                    key={testimony.id}
-                    className="bg-[#18181b] border border-[#282828] p-2 rounded-lg"
-                  >
-                    <p>{testimony.name}</p>
-                    <p>{testimony.role}</p>
-                    <p>{testimony.testimony}</p>
-                  </div>
-                ))
-            )}
+            <TestimonialCard userCategory="Crypto Investor" />
           </div>
         </div>
-        <div className="relative h-[50vh] overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+        <div className="relative  overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
           {/* Content to scroll */}
           <div className="absolute top-0 left-0 w-full animate-marquee space-y-10 py-5">
-            {testimonialsItem.map(
-              (item) =>
-                item.category === "Beginners & Casual Users" &&
-                item.testimonies.map((testimony) => (
-                  <div
-                    key={testimony.id}
-                    className="bg-[#18181b] border border-[#282828] p-2 rounded-lg "
-                  >
-                    <p>{testimony.name}</p>
-                    <p>{testimony.role}</p>
-                    <p>{testimony.testimony}</p>
-                  </div>
-                ))
-            )}
+            <TestimonialCard userCategory="Beginners & Casual Users" />
           </div>
           <div className="absolute top-0 left-0 w-full animate-marquee2 space-y-10 py-5">
-            {testimonialsItem.map(
-              (item) =>
-                item.category === "Beginners & Casual Users" &&
-                item.testimonies.map((testimony) => (
-                  <div
-                    key={testimony.id}
-                    className="bg-[#18181b] border border-[#282828] p-2 rounded-lg"
-                  >
-                    <p>{testimony.name}</p>
-                    <p>{testimony.role}</p>
-                    <p>{testimony.testimony}</p>
-                  </div>
-                ))
-            )}
+            <TestimonialCard userCategory="Beginners & Casual Users" />
           </div>
         </div>
-        <div className="relative h-[50vh] overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+        <div className="relative  overflow-hidden [mask-image:_linear-gradient(to_top,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
           {/* Content to scroll */}
           <div className="absolute top-0 left-0 w-full animate-marquee space-y-10 py-5">
-            {testimonialsItem.map(
-              (item) =>
-                item.category === "Developers and Analysts" &&
-                item.testimonies.map((testimony) => (
-                  <div
-                    key={testimony.id}
-                    className="bg-[#18181b] border border-[#282828] p-2 rounded-lg"
-                  >
-                    <p>{testimony.name}</p>
-                    <p>{testimony.role}</p>
-                    <p>{testimony.testimony}</p>
-                  </div>
-                ))
-            )}
+            <TestimonialCard userCategory="Developers and Analysts" />
           </div>
           <div className="absolute top-0 left-0 w-full animate-marquee2 space-y-10 py-5">
-            {testimonialsItem.map(
-              (item) =>
-                item.category === "Developers and Analysts" &&
-                item.testimonies.map((testimony) => (
-                  <div
-                    key={testimony.id}
-                    className="bg-[#18181b] border border-[#282828] p-2 rounded-lg"
-                  >
-                    <p>{testimony.name}</p>
-                    <p>{testimony.role}</p>
-                    <p>{testimony.testimony}</p>
-                  </div>
-                ))
-            )}
+            <TestimonialCard userCategory="Developers and Analysts" />
           </div>
         </div>
       </div>
