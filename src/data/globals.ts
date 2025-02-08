fbd03c7db9aee891aa88498c4fbfdaa0fa16ad3b
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-08 23:22:17 +0800

    refactor: data gor globas

--- src/data/globals.ts
+++ src/data/globals.ts
@@ -1,6 +1,9 @@
 import { Clock, ChartNoAxesCombined, BetweenVerticalEnd } from "lucide-react";
-import test from "node:test";
+import { Testimony } from "../interfaces/interface";
 import React from "react";
+import Crypto1 from "../../assets/images/landing/profile-pic/crypto-1.jpg";
+import Crypto2 from "../../assets/images/landing/profile-pic/crypto-2.jpg";
+import Crypto3 from "../../assets/images/landing/profile-pic/crypto-3.jpg";
 export const navItems = [
   {
     id: 1,
@@ -51,31 +54,34 @@ export const featureItems = [
   },
 ];
 
-export const testimonialsItem = [
+export const testimonialsItem: Testimony[] = [
   // Testimonials for Crypto Investor
   {
     category: "Crypto Investor",
     testimonies: [
       {
         id: 1,
-        name: " Mark R.",
+        name: "  Carlos D.",
         role: "Crypto Investor",
         testimony:
           "Crypto Daily keeps me updated with real-time prices and trending news. A must-have for any trader!",
+        profile: "../../src/assets/images/landing/profile-pic/crypto-2.jpg",
       },
       {
         id: 2,
-        name: "Jessica T.",
+        name: "Mark R.",
         role: "Day Trader",
         testimony:
           "I love how easy it is to track my favorite coins. The UI is clean and fast!",
+        profile: "../../src/assets/images/landing/profile-pic/crypto-1.jpg",
       },
       {
         id: 3,
-        name: "Carlos D. ",
+        name: "Jessica T. ",
         role: "Swing Trader",
         testimony:
           "The Hot Crypto feature helps me spot market trends before they explode. Highly recommended!",
+        profile: "../../src/assets/images/landing/profile-pic/crypto-3.jpg",
       },
     ],
   },
@@ -89,6 +95,7 @@ export const testimonialsItem = [
         role: "New Crypto Enthusiast",
         testimony:
           "I used to struggle with tracking multiple cryptos, but Crypto Daily makes it effortless!",
+        profile: "../../src/assets/images/landing/profile-pic/beginner-1.jpg",
       },
       {
         id: 2,
@@ -96,6 +103,7 @@ export const testimonialsItem = [
         role: "Beginner Investor",
         testimony:
           "I’m new to crypto, and this app helps me stay informed without feeling overwhelmed.",
+        profile: "../../src/assets/images/landing/profile-pic/beginner-2.jpg",
       },
       {
         id: 3,
@@ -103,6 +111,7 @@ export const testimonialsItem = [
         role: "Casual User",
         testimony:
           "The daily news section helps me understand market movements in simple terms. Love it!",
+        profile: "../../src/assets/images/landing/profile-pic/beginner-3.jpg",
       },
     ],
   },
@@ -116,6 +125,7 @@ export const testimonialsItem = [
         role: "Blockchain Dev",
         testimony:
           "As a blockchain developer, I appreciate the real-time data and smooth UI of Crypto Daily.",
+        profile: "../../src/assets/images/landing/profile-pic/dev-2.jpg",
       },
       {
         id: 2,
@@ -123,6 +133,7 @@ export const testimonialsItem = [
         role: " Data Analyst",
         testimony:
           "The crypto monitoring dashboard is packed with valuable insights for traders and analysts alike!",
+        profile: "../../src/assets/images/landing/profile-pic/dev-1.jpg",
       },
       {
         id: 3,
@@ -130,6 +141,7 @@ export const testimonialsItem = [
         role: "Financial Analyst",
         testimony:
           "I've used many crypto trackers, but Crypto Daily stands out with its ease of use and accuracy.",
+        profile: "../../src/assets/images/landing/profile-pic/dev-3.jpg",
       },
     ],
   },
