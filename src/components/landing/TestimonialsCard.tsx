a24a536c3dcd1e974a6a5b5e3b1689c83899e24a
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-08 23:21:53 +0800

    feat: testimonial card for reusability of testimonials in each users

---
+++ src/components/landing/TestimonialsCard.tsx
@@ -0,0 +1,33 @@
+import { testimonialsItem } from "../../data/globals";
+import { Card, CardHeader, CardDescription, CardContent } from "../common/Card";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+
+export function TestimonialCard({ userCategory }: { userCategory: string }) {
+  return (
+    <>
+      {testimonialsItem
+        .filter((item) => item.category === userCategory)
+        .flatMap((item) =>
+          item.testimonies.map((testimony) => (
+            <Card key={testimony.id} className="space-y-4">
+              <CardContent className="flex flex-row gap-4">
+                <Avatar>
+                  <AvatarImage
+                    src={testimony.profile}
+                    className="h-14 w-14 object-cover rounded-full"
+                  />
+                </Avatar>
+                <CardHeader>
+                  <p className="text-lg font-normal">{testimony.name}</p>
+                  <p className="text-small  text-[#d4d4d4]">{testimony.role}</p>
+                </CardHeader>
+              </CardContent>
+              <CardDescription>
+                <p>{testimony.testimony}</p>
+              </CardDescription>
+            </Card>
+          ))
+        )}
+    </>
+  );
+}
