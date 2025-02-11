bd2381644f29c19fcce2c9cac405875a156cd709
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-11 22:18:10 +0800

    debugging

--- src/components/landing/TrendingCryptoCoin.tsx
+++ src/components/landing/TrendingCryptoCoin.tsx
@@ -6,7 +6,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "../common/Table";
+} from "../../components/common/Table";
 import { useEffect, useState } from "react";
 import { CoinResponse, Coin } from "../../interfaces/interface";
 import { formattedPrice } from "../../utils/utils";
