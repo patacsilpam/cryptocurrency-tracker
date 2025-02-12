35336c46044dcd0f053d94508eaafd1de80e2a5f
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-12 23:21:50 +0800

    feat: added dashboard route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/landing/Home";
+import path from "path";
+import { Dashboard } from "../pages/dashboard/Dashboard";
 export const routes = [
   {
     //public routes with landing page layout
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
   },
 ];
 export const router = createBrowserRouter(routes);
