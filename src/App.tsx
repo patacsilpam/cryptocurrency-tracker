7ccfeb8a3873e599b932f2ee1675648d0a341532
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-12 23:23:37 +0800

    --no update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/routes";
+
 function App() {
   return (
     <div className="relative flex flex-col justify-center overflow-hidden max-w-7xl w-full mx-auto px-10  text-white">
