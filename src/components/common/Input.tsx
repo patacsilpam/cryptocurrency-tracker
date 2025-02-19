47698e3104cabf94e797cf6d89c576d1db0c0c3e
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-19 22:12:26 +0800

    feat: Input with label component

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -7,8 +7,19 @@ type InputProps = {
 export function Input({ label, type, onChange, ...props }: InputProps) {
   return (
     <>
-      <label>{label}</label>
-      <input type={type} onChange={onChange} {...props} />
+      <form
+        action=""
+        className="flex flex-col space-y-1"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <label className="font-medium">{label}</label>
+        <input
+          type={type}
+          onChange={onChange}
+          {...props}
+          className="p-3 rounded-lg outline-none bg-zinc-800 hover:outline hover:outline-zinc-800"
+        />
+      </form>
     </>
   );
 }
