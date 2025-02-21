ccf78748ec8d004fd57a5db684a69f9732310bbd
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-22 06:34:44 +0800

    refactor: changed form tag to div tag for proper wrapping

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -7,19 +7,15 @@ type InputProps = {
 export function Input({ label, type, onChange, ...props }: InputProps) {
   return (
     <>
-      <form
-        action=""
-        className="flex flex-col space-y-1"
-        onSubmit={(e) => e.preventDefault()}
-      >
+      <div className="flex flex-col space-y-1">
         <label className="font-medium">{label}</label>
         <input
           type={type}
           onChange={onChange}
           {...props}
-          className="p-3 rounded-lg outline-none bg-zinc-800 hover:outline hover:outline-zinc-800"
+          className="p-3  outline-none bg-zinc-800 hover:outline hover:outline-zinc-800"
         />
-      </form>
+      </div>
     </>
   );
 }
