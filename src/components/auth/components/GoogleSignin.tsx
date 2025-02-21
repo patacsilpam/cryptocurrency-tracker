9600485f86d0f5cf8068faf19bb1c8c40fb8dc01
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-22 06:56:18 +0800

    feat: redirect to dashboard after login

--- src/components/auth/components/GoogleSignin.tsx
+++ src/components/auth/components/GoogleSignin.tsx
@@ -1,24 +1,31 @@
 import { supabase } from "@/config/supabaseClient";
 import Google from "@/assets/icons/google.png";
-export function GoogleSignin() {
+import { ReactNode } from "react";
+
+interface GoogleSigninProps {
+  children: ReactNode;
+}
+
+export function GoogleSignin({ children }: GoogleSigninProps) {
   const signIn = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin, // Correct: Redirect to your app's URL
+        redirectTo: window.location.origin + "/dashboard", // Correct: Redirect to your app's URL
       },
     });
     if (error) {
       console.error("Error signing in with Google", error);
     }
   };
+
   return (
     <button
       onClick={signIn}
       className="flex justify-center items-center gap-x-4 border border-zinc-700 text-center text-md rounded-full w-full p-3"
     >
       <img src={Google} alt="Google Icon" className=" h-[15px]" />
-      Sign in with Google
+      {children}
     </button>
   );
 }
