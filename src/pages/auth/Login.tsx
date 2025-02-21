a64bfcc901767b6ff9791860fbd2b7751b317154
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-22 06:32:59 +0800

    style: removed redundant code for better styling

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -5,12 +5,13 @@ import { Input } from "@/components/common/Input";
 export function Login() {
   return (
     <div className="flex items-center justify-center">
-      <div className="shadow-lg rounded-lg p-5 max-w-md w-full">
+      <div className="shadow-lg rounded p-5 max-w-md w-full bg-zinc-900">
         {/* Align text to the left */}
-        <div className="w-full space-y-6">
-          <h1 className="text-xl md:text-2xl font-bold text-center">
-            Sign in to Crypto Daily
-          </h1>
+        <h1 className="text-xl md:text-2xl font-bold text-center">
+          Sign in to Crypto Daily
+        </h1>
+        {/*Google Sign in */}
+        <div className="w-full space-y-4 mt-5">
           <GoogleSignin />
           <div className="flex items-center w-full">
             <hr className="flex-grow border-gray-300" />
@@ -20,8 +21,12 @@ export function Login() {
             <hr className="flex-grow border-gray-300" />
           </div>
         </div>
-
-        <div className="space-y-7 mt-5">
+        <form
+          className="space-y-5"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
           <Input
             placeholder="Email"
             type="email"
@@ -38,18 +43,18 @@ export function Login() {
 
           <Button
             type="submit"
-            className="bg-white w-full font-medium text-gray-900 rounded-full p-3"
+            className="bg-white w-full font-medium text-gray-900 rounded-md p-3"
           >
             Login
           </Button>
+        </form>
 
-          <span className="block text-center text-gray-600">
-            Don't have an account?{" "}
-            <a href="/register" className="text-blue-600 underline">
-              Sign up
-            </a>
-          </span>
-        </div>
+        <span className="block text-center text-gray-600 mt-3">
+          Don't have an account?{" "}
+          <a href="/auth/register" className="text-gray-300 underline">
+            Sign up
+          </a>
+        </span>
       </div>
     </div>
   );
