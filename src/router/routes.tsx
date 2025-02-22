9236ed4b1d313c82a5c2fdadb3cf65dd64c43c8f
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-02-23 07:35:32 +0800

    feat(auth): implement private route to restrict access for unauthenticated users

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,17 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Home } from "../pages/landing/Home";
 import { Dashboard } from "../pages/dashboard/Dashboard";
 import { Login } from "@/pages/auth/Login";
 import { Register } from "@/pages/auth/Register";
+import { useEffect, useState } from "react";
+import { supabase } from "@/config/supabaseClient";
+
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const [session, setSession] = useState<boolean | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(!!session);
+      setLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(!!session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loadding...</div>;
+  }
+  return session ? children : <Navigate to="/auth/login" replace />;
+};
+
 export const routes = [
   {
-    //public routes with landing page layout
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
   },
   {
     path: "/auth/login",
@@ -21,5 +43,15 @@ export const routes = [
     path: "/auth/register",
     element: <Register />,
   },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <Dashboard />
+      </PrivateRoute>
+    ),
+  },
+  { path: "*", element: <Navigate to="/auth/login" replace /> },
 ];
+
 export const router = createBrowserRouter(routes);
