17339f807f2982906d13245dc41ff3eceb294cac
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-04-11 23:06:06 +0800

    fix: adjust text to large size for readability

--- src/components/common/Tabs.tsx
+++ src/components/common/Tabs.tsx
@@ -25,7 +25,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "relative inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      "relative inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-lg font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
       "data-[state=active]:text-blue-600", // Change text color for active tab
       "after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-blue-600 after:scale-x-0 after:transition-transform after:duration-300", // Underline styles
       "data-[state=active]:after:scale-x-100", // Show underline when active
